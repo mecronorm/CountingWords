525f716209650d726699f98393a77cc5eba0e69d
mecronorm <alessandropaap@hotmail.com>
2024-01-03 11:09:49 +0100

    Created the word count using Map and HashMap to group unique words with their count

--- src/CountingWords.java
+++ src/CountingWords.java
@@ -1,22 +1,47 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CountingWords {
 	public static void main(String[] args) {
-		try {
-			File myText = new File("C:\\Users\\alessandro\\eclipse-workspace\\CountingWords\\src\\Story.txt");
-			Scanner myReader = new Scanner(myText);
-			while(myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				System.out.println(data);
+		Map<String, Integer> wordCount = new HashMap<>();
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader("Story.txt"))){
+			String line;
+			
+			while((line = reader.readLine()) != null) {
+				String[] words = line.split("\\s+");
+				
+				for (String word : words) {
+					word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+					
+					wordCount.put(word, wordCount.getOrDefault(word, 0)+1);
+				}
 			}
-			myReader.close();
+			reader.close();
 		}
-		catch(FileNotFoundException e) {
+		catch(IOException e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
+		String mostCommon = null;
+		int maxCount = 0;
+		
+		System.out.println("Word Count:");
+		for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+			if (entry.getValue() > maxCount) {
+				mostCommon = entry.getKey();
+				maxCount = entry.getValue();
+			}
+		}
+		
+		if(mostCommon != null) {
+			System.out.println("\nThe most used word in this text is: " + mostCommon);
+		} else {
+			System.out.println("There are no words.");
+		}
 	}
-
 }
